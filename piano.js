cb585ef341fa95f44ffec1dd18468d76e9a0c8c8
shyam-77 <72121828+shyam-77@users.noreply.github.com>
2021-03-17 20:19:15 +0530

    Add files via upload
    completed

---
+++ piano.js
@@ -0,0 +1,20 @@
+var Apanio = new Audio()
+Apanio.src ="A.wav";
+
+var Bpanio = new Audio()
+Bpanio.src ="B.wav";
+
+var Cpanio = new Audio()
+Cpanio.src ="C.wav";
+
+var Dpanio = new Audio()
+Dpanio.src ="D.wav";
+
+var Epanio = new Audio()
+Epanio.src ="E.wav";
+
+var Fpanio = new Audio()
+Fpanio.src ="F.wav";
+
+var Apanio = new Audio()
+Apanio.src ="A.wav";
